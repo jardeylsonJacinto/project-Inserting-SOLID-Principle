5bf6af23c6e674fc12fae52bb72defbd2b3a3a22
Jardeylson jacinto <jardeylsong.m@gmail.com>
2023-01-02 15:01:07 -0300

    feat: Implementing the turnAdmin method

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -46,7 +46,12 @@ class UsersRepository implements IUsersRepository {
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const user = receivedUser;
+
+    user.admin = true;
+    user.updated_at = new Date();
+
+    return user;
   }
 
   list(): User[] {
