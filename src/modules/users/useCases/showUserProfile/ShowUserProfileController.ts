d212b49fb9e01e09609994332f6ac589f298b0f0
Jardeylson jacinto <jardeylsong.m@gmail.com>
2023-01-02 17:01:54 -0300

    feat: Implementing the handle of the show user profile controller

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -3,10 +3,17 @@ import { Request, Response } from "express";
 import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
 
 class ShowUserProfileController {
-  constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
+  constructor(private showUserProfileUseCase: ShowUserProfileUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    try {
+      const user = this.showUserProfileUseCase.execute({ user_id });
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err });
+    }
   }
 }
 
